f272bafdb8ad99b178c3a90b1fb79ebb29663eca
nirajsathe80 <nirajsathe80@gmail.com>
2024-08-08 21:21:16 +0530

    homepage add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import CardList from "@/components/card-list";
+import Loader from "@/components/common/loader";
+import Banner from "@/components/home-banner";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const response = await fetch(
+    "https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=24"
+  );
+  if (!response.ok) {
+    throw new Error("Error fetching");
+  }
+  const data = await response.json();
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="max-w-6xl m-auto">
+      <Banner />
+      <Suspense fallback={<Loader />}>
+        <CardList books={data?.items} />
+      </Suspense>
+    </div>
   );
 }
